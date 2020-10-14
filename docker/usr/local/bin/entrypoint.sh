0ce94e849723fe86dfaf473985e120771ede3f7c
ravenstar <ravenstar@users.noreply.github.com>
2020-10-14 16:28:12 +1000

    Update entrypoint.sh
    Now utilises the PGID and PUID env variables to set the "nginx" UID and GID.

--- docker/usr/local/bin/entrypoint.sh
+++ docker/usr/local/bin/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-PUID=${PUID:-911}
-PGID=${PGID:-911}
-
 groupmod -o -g "$PGID" nginx
 usermod -o -u "$PUID" nginx
+ 
+echo "********************"
+echo " nginx user:"
+echo "  UID: ${PUID}"
+echo "  GID: ${PGID}"
 
 # Generate a new self signed SSL certificate when none is provided in the volume
 if [ ! -f /etc/nginx/ssl/resourcespace.key  ] || [ ! -f /etc/nginx/ssl/resourcespace.crt ]
 then
+    echo "********************"
     openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/nginx/ssl/resourcespace.key -out /etc/nginx/ssl/resourcespace.crt -subj "/C=GB/ST=London/L=London/O=Self Signed/OU=IT Department/CN=resourcespace.org"
 fi
 
-chown -R nginx:nginx /var/www/app/.
+echo "********************"
+echo " Running chown on app dir... this may take awhile..."
+time chown -R nginx:nginx /var/www/app/. /var/tmp/nginx/
 
 exec /bin/s6-svscan /etc/services.d
